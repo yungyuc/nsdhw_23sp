1f8911c6b167859a7b7efe6492b7f536062330de
Billy <nibilly118@gmail.com>
2023-02-18 15:23:31 +0800

    bruh

--- hw1/BillyNI-OUO/countline.sh
+++ hw1/BillyNI-OUO/countline.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
-  echo "missing file name"
-elif [[ $# -gt 1 ]] ; then
-  echo "only one argument is allowed"
-else
-    if [[ -f $1 ]] ; then
-      echo "$(grep -c ^ $1) lines in $1"
+if [ $# -lt 1 ] ; then
+    echo "missing file name"
+elif [ $# -gt 1 ] ; then
+    echo "only one argument is allowed"
+else 
+    fname=$1
+    if [ -f $fname ]; then
+        lines="$(grep -c ^ $1)"
+        echo "$lines lines in $fname"
     else
-      echo "$1 not found"
+        echo "$fname not found"
     fi
 fi
