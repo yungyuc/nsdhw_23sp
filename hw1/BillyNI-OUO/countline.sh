eeef6953bd19c4921de5964c9167f87e292a6545
nibilly <nibilly118@gmail.com>
2023-02-20 07:25:19 +0800

    fix permisson

--- hw1/BillyNI-OUO/countline.sh
+++ hw1/BillyNI-OUO/countline.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ $# -lt 1 ] ; then
+if [ "$#" -lt 1 ]; then
     echo "missing file name"
-elif [ $# -gt 1 ] ; then
+elif [ "$#" -gt 1 ]; then
     echo "only one argument is allowed"
-else 
-    fname=$1
-    if [ -f $fname ]; then
-        lines="$(grep -c ^ $1)"
-        echo "$lines lines in $fname"
+else
+    FNAME=$1
+    if [ -f "$FNAME" ]; then
+        LINES=`wc -l < "$FNAME"`
+        echo "$LINES lines in $FNAME"
     else
-        echo "$fname not found"
+        echo "$FNAME not found"
     fi
 fi
