7fc35d32ef7162f297452cf67542dddca42450fe
Eric <Eric@eda14.(none)>
2023-02-21 15:02:37 +0800

    initial commit

---
+++ hw1/yoyojs200602/countline.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import subprocess
+
+PyBin = os.environ['PYTHON_BIN']
+
+try:
+    subprocess.check_output([PyBin, '--version'], stderr=subprocess.STDOUT)
+except (subprocess.CalledProcessError, OSError):
+    sys.stderr.write('Error: {} can not be executed !\n'.format(PyBin))
+    sys.exit(1)
+
+if len(sys.argv) < 2:
+    sys.stdout.write('missing file name\n')
+elif len(sys.argv) > 2:
+    sys.stdout.write('only one argument is allowed\n')
+else:
+    fname = sys.argv[1]
+    if os.path.exists(fname):
+        with open(fname) as fobj:
+            lines = fobj.readlines()
+        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+    else:
+        sys.stdout.write('{} not found\n'.format(fname))
