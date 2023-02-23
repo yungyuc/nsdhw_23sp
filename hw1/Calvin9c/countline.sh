c7c0343528ddfe89ae88a05e68c20e78295bb0b9
Calvin9c <m.acc.ch1230@gmail.com>
2023-02-23 15:00:47 +0800

    upload countline.py

--- hw1/Calvin9c/countline.sh
+++ hw1/Calvin9c/countline.sh
@@ -10,7 +10,7 @@ else
   if [ -e fname]; then # -e fname will return true if file exist
     num_lines=$(wc -l < $fname) # wc will return three numbers which stand for the number of line, word and byte respectively. 
     echo "$num_lines lines in $fname\n"
-  else
+    
     echo "$fname not found\n"
   fi
 fi
