96f820385a5d5c7ae445d712bc1657e02820040d
mmi366127 <mmi366127@gmail.com>
2023-02-23 19:02:17 +0800

    modify countline.py and add countline.sh

--- hw1/mmi366127/countline.py
+++ hw1/mmi366127/countline.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env -S ${PYTHON_BIN}
+#!/usr/bin/env python3
 
 import sys
 import os.path
 
-if len(sys.argv) < 2:
-    sys.stdout.write('missing file name\n')
-elif len(sys.argv) > 2:
-    sys.stdout.write('only one argument is allowed\n')
-else:
-    fname = sys.argv[1]
-    if os.path.exists(fname):
-        with open(fname) as fobj:
-            lines = fobj.readlines()
-        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
-    else:
-        sys.stdout.write('{} not found\n'.format(fname))
+PYTHON_BIN = os.environ['PYTHON_BIN'] if 'PYTHON_BIN' in os.environ.keys() else 'python3'
+
+argv=f'argv = {str(sys.argv)}'
+code = f"\"import sys\\n" + argv +  "\\nimport os.path\\nif len(argv) < 2:\\n    print(\'missing file name\')\\nelif len(argv) > 2:\\n    print(\'only one argument is allowed\\\n\')\\nelse:\\n    fname = argv[1]\\n    if os.path.exists(fname):\\n        with open(fname) as fobj:\\n            lines = fobj.readlines()\\n        print(\'{} lines in {}\\\n\'.format(len(lines), fname))\\n    else:\\n        print(\'{} not found\\\n\'.format(fname))\""
+
+ret = os.system(f'echo {code} | exec {PYTHON_BIN}')
+if ret != 0 : ret = 1
+exit(ret)
+
