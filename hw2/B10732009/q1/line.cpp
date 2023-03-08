a0ce5892dc376f8d2122b773b93b84d9dc4d6d5c
B10732009 <yuchen4667@yahoo.com.tw>
2023-03-08 16:37:39 +0800

    Add q1

---
+++ hw2/B10732009/q1/line.cpp
@@ -0,0 +1,49 @@
+#include "line.hpp"
+
+// default contructor
+Line::Line() : dataSize(0), dataX({}), dataY({}){};
+
+// copy constructor
+Line::Line(const Line &line) : dataSize(line.dataSize), dataX(line.dataX), dataY(line.dataY) {}
+
+// move constructor
+Line::Line(Line &&line) : dataSize(line.dataSize), dataX(std::move(line.dataX)), dataY(std::move(line.dataY)) {}
+
+// copy assignment operator
+Line &Line::operator=(Line const &line)
+{
+    // self-assignment check
+    if (this != &line)
+    {
+        dataSize = line.dataSize;
+        dataX = line.dataX;
+        dataY = line.dataY;
+    }
+    return *this;
+}
+
+// move assignment operator
+Line &Line::operator=(Line &&line)
+{
+    // self-assignment check
+    if (this != &line)
+    {
+        dataSize = line.dataSize;
+        dataX = std::move(line.dataX);
+        dataY = std::move(line.dataY);
+    }
+    return *this;
+}
+
+// custom constructor
+Line::Line(size_t _dataSize) : dataSize(_dataSize), dataX(std::vector<double>(_dataSize)), dataY(std::vector<double>(_dataSize)) {}
+
+// destructor
+Line::~Line() {}
+
+// accessors
+std::size_t Line::size() const { return dataSize; }
+double const &Line::x(std::size_t it) const { return dataX.at(it); }
+double &Line::x(std::size_t it) { return dataX.at(it); }
+double const &Line::y(std::size_t it) const { return dataY.at(it); }
+double &Line::y(std::size_t it) { return dataY.at(it); }
---
+++ hw2/B10732009/q1/line.hpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+class Line
+{
+public:
+    Line();                        // default contructor
+    Line(Line const &);            // copy constructor
+    Line(Line &&);                 // move constructor
+    Line &operator=(Line const &); // copy assignment operator
+    Line &operator=(Line &&);      // move assignment operator
+    ~Line();                       // destructor
+
+    Line(std::size_t size);
+    std::size_t size() const;
+    double const &x(std::size_t it) const;
+    double &x(std::size_t it);
+    double const &y(std::size_t it) const;
+    double &y(std::size_t it);
+
+private:
+    // Member data.
+    std::size_t dataSize;
+    std::vector<double> dataX;
+    std::vector<double> dataY;
+}; /* end class Line */
