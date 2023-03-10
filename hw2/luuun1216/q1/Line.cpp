f79c6dc123de8c8d2c1d5fc0ec1d13bb5a5c3dfe
luuun1216 <allen.yu1216@gmail.com>
2023-03-10 17:58:38 +0800

    upload q1 floder

---
+++ hw2/luuun1216/q1/Line.cpp
@@ -0,0 +1,31 @@
+# include "Line.hpp"
+# include <iostream>
+
+Line::Line(){}
+
+Line::Line(size_t size){
+    this->vector_x.assign(size,0);
+    this->vector_y.assign(size,0);
+}
+
+Line::~Line(){}
+
+size_t Line::size() const{
+    return this->vector_x.size();
+}
+
+const float & Line::x(size_t it) const{
+    return this->vector_x.at(it);
+}
+
+float & Line::x(size_t it){
+    return this->vector_x.at(it);
+}
+
+const float & Line::y(size_t it) const{
+    return this->vector_y.at(it);
+}
+
+float & Line::y(size_t it){
+    return this->vector_y.at(it);
+}
