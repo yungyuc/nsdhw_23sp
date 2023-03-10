fba227cabaa574d279c45955566f791434470144
luuun1216 <allen.yu1216@gmail.com>
2023-03-10 18:25:23 +0800

    fix Line.cpp

--- hw2/luuun1216/q1/Line.cpp
+++ hw2/luuun1216/q1/Line.cpp
@@ -2,12 +2,40 @@
 # include <iostream>
 
 Line::Line(){}
+
+Line::Line(Line const & line){
+    this->vector_x.assign(line.vector_x.begin(), line.vector_x.end());
+    this->vector_y.assign(line.vector_y.begin(), line.vector_y.end());
+}
+
+Line::Line(Line && line){
+    this->vector_x.assign(line.vector_x.begin(), line.vector_x.end());
+    this->vector_y.assign(line.vector_y.begin(), line.vector_y.end());
+}
+
+Line & Line::operator = (Line const & line){
+    vector_x = line.vector_x;
+    vector_y = line.vector_y;
+
+    return *this;
+}
+
+Line & Line::operator = (Line && line){
+    vector_x = line.vector_x;
+    vector_y = line.vector_y;
+
+    return *this;
+}
+
 Line::Line(size_t size){
     this->vector_x.assign(size,0);
     this->vector_y.assign(size,0);
 }
 
-Line::~Line(){}
+Line::~Line(){
+    this->vector_x.clear();
+    this->vector_y.clear();
+}
 
 size_t Line::size() const{
     return this->vector_x.size();
