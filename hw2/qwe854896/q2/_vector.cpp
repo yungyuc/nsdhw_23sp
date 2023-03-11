d9cdee28eedce3f345ece9ec62f161c49250eea1
Jun-Hong Cheng <qwe854896@gmail.com>
2023-03-11 14:14:39 +0000

    fix: prototype of vector is set to [] or np.array
    - C++: vector<double>

--- hw2/qwe854896/q2/_vector.cpp
+++ hw2/qwe854896/q2/_vector.cpp
@@ -1,17 +1,26 @@
+#include <pybind11/stl.h>
 #include <pybind11/pybind11.h>
 #include <cmath>
 using namespace std;
 namespace py = pybind11;
 
-double angle(double x1, double y1, double x2, double y2)
+double angle_between(const vector<double> &v1, const vector<double> &v2)
 {
-    return acos((x1 * x2 + y1 * y2) / sqrt((x1 * x1 + y1 * y1) * (x2 * x2 + y2 * y2)));
+    if (v1.size() != 2 || v2.size() != 2)
+    {
+        throw std::invalid_argument("vector is not in the 2-dimensional.");
+    }
+    if (((v1[0] == 0.0) && (v1[1] == 0.0)) || ((v2[0] == 0.0) && (v2[1] == 0.0)))
+    {
+        throw std::invalid_argument("vector can't be zero length");
+    }
+    return acos((v1[0] * v2[0] + v1[1] * v2[1]) / sqrt((v1[0] * v1[0] + v1[1] * v1[1]) * (v2[0] * v2[0] + v2[1] * v2[1])));
 }
 
 PYBIND11_MODULE(_vector, m)
 {
     m.doc() = "_vector package";
-    m.def("angle",
-          &angle,
-          "calculates the angle (in radians) between two vectors in the 2-dimensional Cartesian coordinate system.");
+    m.def("angle_between",
+          &angle_between,
+          "Calculates the angle (in radians) between two vectors in the 2-dimensional Cartesian coordinate system.");
 }
--- hw2/qwe854896/q2/test_vector.py
+++ hw2/qwe854896/q2/test_vector.py
@@ -1,22 +1,51 @@
+import numpy as np
+import numpy.ma as ma
+import math
+
 import _vector
-from math import pi
 
 
-def feq(x, y, eps=1e-6):
-    return abs(x - y) < eps
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'.  """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
+def feq(x, y):
+    return ma.allclose(x, y)
 
 
 def test_zero_length():
-    assert _vector.angle(0, 0, 0, 0)
+    try:
+        _vector.angle_between(0, 0, 0, 0)
+        assert False
+    except:
+        assert True
 
 
 def test_zero_angle():
-    assert feq(_vector.angle(3, 4, 3, 4), 0.0)
+    for _ in range(10):
+        v = np.random.rand(2)
+        assert feq(_vector.angle_between(v, v), 0.0)
 
 
 def test_right_angle():
-    assert feq(_vector.angle(3, 4, -4, 3), pi / 2.0)
+    RIGHT_ANGLE = math.pi / 2.0
+    for _ in range(10):
+        v1 = np.random.rand(2)
+        v2 = np.zeros_like(v1)
+        v2[0], v2[1] = -v1[1], v1[0]
+        assert feq(_vector.angle_between(v1, v2), RIGHT_ANGLE)
 
 
 def test_random_angle():
-    assert feq(_vector.angle(1, 0, 1, 1),  pi / 4.0)
+    for _ in range(10):
+        v1 = np.random.rand(2)
+        v2 = np.random.rand(2)
+        assert feq(_vector.angle_between(v1, v2),  angle_between(v1, v2))
