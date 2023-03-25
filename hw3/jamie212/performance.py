789c5347c59d22a67354c22be4d703c429ccf192
jamie212 <jamiewangtw2012@gmail.com>
2023-03-26 02:40:55 +0800

    hw3 attempt, performance.py

--- hw3/jamie212/performance.py
+++ hw3/jamie212/performance.py
@@ -4,13 +4,16 @@ import timeit
 def benchmark():
     setup = '''
 import _matrix
+
 size = 1000
+
 mat1 = _matrix.Matrix(size,size)
 mat2 = _matrix.Matrix(size,size)
-for i in range(size):
-    for j in range(size):
-        mat1[i, j] = 2 * (i ** 2) + j + 2.0
-        mat2[i, j] = 2 * (j ** 2) + i + 2.0
+
+for it in range(size):
+    for jt in range(size):
+        mat1[it, jt] = it * size + jt + 1
+        mat2[it, jt] = it * size + jt + 1
 '''
 
     repeat = 5
@@ -23,17 +26,22 @@ for i in range(size):
         f.write('Start multiply_naive (repeat=5), take min = ')
         naiveTime = min(mul_naive.repeat(repeat=repeat, number=1))
         f.write('{} seconds\n'.format(naiveTime))
+        print('Start multiply_naive (repeat=5), take min = {} seconds\n'.format(naiveTime))
 
         f.write('Start multiply_tile (repeat=5), take min = ')
         tileTime = min(mul_tile.repeat(repeat=repeat, number=1))
         f.write('{} seconds\n'.format(tileTime))
+        print('Start multiply_tile (repeat=5), take min = {} seconds\n'.format(tileTime))
         
         f.write('Start multiply_mkl (repeat=5), take min = ')
         mklTime = min(mul_mkl.repeat(repeat=repeat, number=1))
         f.write('{} seconds\n'.format(mklTime))
+        print('Start multiply_mkl (repeat=5), take min = {} seconds\n'.format(mklTime))
 
         f.write('Tile speed-up over naive: {} x\n'.format(tileTime / naiveTime))
+        print('Tile speed-up over naive: {} x\n'.format(tileTime / naiveTime))
         f.write('MKL speed-up over naive: {} x\n'.format(mklTime / naiveTime))
+        print('MKL speed-up over naive: {} x\n'.format(mklTime / naiveTime))
 
 if __name__ == "__main__":
     benchmark()
