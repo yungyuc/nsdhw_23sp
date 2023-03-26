d719169f400ec5ed3b5b08393adcb549b196d8e8
yoyojs200602 <yoyochang24@gmail.com>
2023-03-26 23:22:49 +0800

    timing issue

--- hw3/yoyojs200602/_matrix.cpp
+++ hw3/yoyojs200602/_matrix.cpp
@@ -7,27 +7,16 @@
 
 class Matrix {
 public:
-    Matrix(): nrow_(0), ncol_(0), data_(nullptr) {}
-    Matrix(int nrow, int ncol): nrow_(nrow), ncol_(ncol)
-    {
-        data_ = new double[nrow_ * ncol_];
-        memset(data_, 0, nrow_ * ncol_ * sizeof(double));
-    }
-    Matrix(const Matrix &m): nrow_(m.nrow()), ncol_(m.ncol())
-    {
-        data_ = new double[nrow_ * ncol_];
-        memcpy(data_, m.data(), nrow_ * ncol_ * sizeof(double));
-    }
-
-    ~Matrix() { if (data_) delete[] data_; }
+    Matrix(): nrow_(0), ncol_(0) {}
+    Matrix(int nrow, int ncol): nrow_(nrow), ncol_(ncol), data_(nrow * ncol) {}
 
     int nrow() const { return nrow_; }
     int ncol() const { return ncol_; }
 
     double& operator()(int i, int j) { return data_[i * ncol_ + j]; }
     const double& operator()(int i, int j) const { return data_[i * ncol_ + j]; }
-    double* data() { return data_; }
-    const double* data() const { return data_; }
+    double* data() { return data_.data(); }
+    const double* data() const { return data_.data(); }
     bool operator ==(const Matrix &m) const
     {
         if (this->nrow() != m.nrow() || this->ncol() != m.ncol())
@@ -47,13 +36,13 @@ public:
 
 private:
     int nrow_, ncol_;
-    double* data_;
+    std::vector<double> data_;
 };
 
 Matrix multiply_naive(const Matrix& a, const Matrix& b) {
     if (a.ncol() != b.nrow())
     {
-        throw std::out_of_range("b's column differs from a's row size");
+        throw std::out_of_range("matrix column differs from row size");
     }
 
     Matrix c(a.nrow(), b.ncol());
@@ -72,26 +61,18 @@ Matrix multiply_naive(const Matrix& a, const Matrix& b) {
 }
 
 Matrix multiply_tile(const Matrix& a, const Matrix& b, int tile_size) {
+    Matrix c(a.nrow(), b.ncol());
 
-    int aR = a.nrow(), aC = a.ncol(), bC = b.ncol();
-    Matrix c(aR, bC); 
-
-    for (int i0 = 0; i0 < aR; i0 += tile_size) 
-    {
-        int i1 = std::min(i0 + tile_size, aR);
-        for (int j0 = 0; j0 < bC; j0 += tile_size) 
-        {
-            int j1 = std::min(j0 + tile_size, bC);
-            for (int k0 = 0; k0 < aC; k0 += tile_size) 
-            {
-                int k1 = std::min(k0 + tile_size, aC);
-                for (int i = i0; i < i1; ++i) 
-                {
-                    for (int j = j0; j < j1; ++j) 
-                    {
+    for (int i0 = 0; i0 < a.nrow(); i0 += tile_size) {
+        int i1 = std::min(i0 + tile_size, a.nrow());
+        for (int j0 = 0; j0 < b.ncol(); j0 += tile_size) {
+            int j1 = std::min(j0 + tile_size, b.ncol());
+            for (int k0 = 0; k0 < a.ncol(); k0 += tile_size) {
+                int k1 = std::min(k0 + tile_size, a.ncol());
+                for (int i = i0; i < i1; ++i) {
+                    for (int j = j0; j < j1; ++j) {
                         double sum = 0.0;
-                        for (int k = k0; k < k1; ++k) 
-                        {
+                        for (int k = k0; k < k1; ++k) {
                             sum += a(i, k) * b(k, j);
                         }
                         c(i, j) += sum;
@@ -110,7 +91,7 @@ Matrix multiply_mkl(const Matrix& a, const Matrix& b) {
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
                 a.nrow(), b.ncol(), a.ncol(), 1.0,
                 a.data(), a.ncol(), b.data(), b.ncol(),
-                0.0, c.data(), c.ncol());
+                0.0, const_cast<double*>(c.data()), c.ncol());
 
     return c;
 }
