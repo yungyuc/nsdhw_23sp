04af731ce2899c0e27d9181fb654a0d7d8866843
yoyojs200602 <yoyochang24@gmail.com>
2023-03-26 16:49:33 +0800

    perfomance test

--- hw3/yoyojs200602/test_multiply.py
+++ hw3/yoyojs200602/test_multiply.py
@@ -89,6 +89,7 @@ def test_tile():
 def test_benchmark():
 
     mat1, mat2, _ = gen_test_matrices(mat_size)
+    mat_size = 1000
     ns = dict(multiply_naive=multiply_naive, multiply_tile=multiply_tile, multiply_mkl=multiply_mkl,
                 mat1=mat1, mat2=mat2, tile_size=tile_size)
 
