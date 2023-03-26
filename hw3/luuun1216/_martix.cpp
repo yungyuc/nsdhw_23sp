275dc4522b3d124ebff7d533f7a8f230b0bda526
luuun1216 <allen.yu1216@gmail.com>
2023-03-26 15:34:03 +0800

    upload file

---
+++ hw3/luuun1216/_martix.cpp
@@ -0,0 +1,57 @@
+#include "_matrix.hpp"
+#include <cstddef>
+#include <algorithm>
+
+Matrix multiply_naive(Matrix const & matrix1, Matrix const & matrix2){
+    Matrix ret(matrix1.nrow(), matrix2.ncol());
+    for (size_t row = 0; row < ret.nrow(); row++){
+        for (size_t column = 0; column < ret.ncol(); column++){
+            double value = 0;
+            for (size_t k = 0; k < matrix1.ncol(); k++){
+                value += matrix1(row, k) * matrix2(k, column);
+            }
+            ret(row, column) = value;
+        }
+    }
+    return ret;
+}
+
+Matrix multiply_tile(Matrix const & matrix1, Matrix const & matrix2, size_t tilesize){
+    Matrix ret(matrix1.nrow(), matrix2.ncol());
+    for (size_t row = 0; row < matrix1.nrow(); row += tilesize){
+        for (size_t col = 0; col < matrix2.ncol(); col += tilesize){
+            for (size_t inner = 0; inner < matrix1.ncol(); inner += tilesize){
+                for (size_t k = inner; k < std::min(matrix1.ncol(), inner + tilesize); k++){
+                    for (size_t i = row; i < std::min(matrix1.nrow(), row + tilesize); i++){
+                        for (size_t j = col; j < std::min(matrix2.ncol(), col + tilesize); j++){
+                            ret(i, j) += matrix1(i, k) * matrix2(k, j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return ret;
+}
+
+Matrix multiply_mkl(Matrix const & matrix1, Matrix const & matrix2){
+    Matrix ret = Matrix(matrix1.nrow(), matrix2.ncol());
+    cblas_dgemm(
+        CblasRowMajor,
+        CblasNoTrans,
+        CblasNoTrans,
+        matrix1.nrow(),
+        matrix2.ncol(),
+        matrix1.ncol(),
+        1.0,
+        matrix1.get_matrix_buffer(),
+        matrix1.ncol(),
+        matrix2.get_matrix_buffer(),
+        matrix2.ncol(),
+        0.0,
+        ret.get_matrix_buffer(),
+        ret.ncol()
+    );
+
+    return ret;
+}
---
+++ hw3/luuun1216/performance.py
@@ -0,0 +1,44 @@
+import _matrix
+import timeit
+
+def benchmark():
+    setup = '''
+import _matrix
+size = 1000
+mat1 = _matrix.Matrix(size,size)
+mat2 = _matrix.Matrix(size,size)
+for it in range(size):
+    for jt in range(size):
+        mat1[it, jt] = it * size + jt + 1
+        mat2[it, jt] = it * size + jt + 1
+'''
+
+    repeat = 5
+
+    naive = timeit.Timer('_matrix.multiply_naive(mat1, mat2)', setup=setup)
+    tile = timeit.Timer('_matrix.multiply_tile(mat1, mat2, 16)', setup=setup)
+    mkl = timeit.Timer('_matrix.multiply_mkl(mat1, mat2)', setup=setup)
+
+    with open('performance.txt', 'w') as f:
+        f.write('Start multiply_naive (repeat=5), take min = ')
+        naiveTime = min(naive.repeat(repeat=repeat, number=1))
+        f.write('{} seconds\n'.format(naiveTime))
+        print('Start multiply_naive (repeat=5), take min = {} seconds\n'.format(naiveTime))
+
+        f.write('Start multiply_tile (repeat=5), take min = ')
+        tileTime = min(tile.repeat(repeat=repeat, number=1))
+        f.write('{} seconds\n'.format(tileTime))
+        print('Start multiply_tile (repeat=5), take min = {} seconds\n'.format(tileTime))
+
+        f.write('Start multiply_mkl (repeat=5), take min = ')
+        mklTime = min(mkl.repeat(repeat=repeat, number=1))
+        f.write('{} seconds\n'.format(mklTime))
+        print('Start multiply_mkl (repeat=5), take min = {} seconds\n'.format(mklTime))
+
+        f.write('Tile speed-up over naive: {} x\n'.format(tileTime / naiveTime))
+        print('Tile speed-up over naive: {} x\n'.format(tileTime / naiveTime))
+        f.write('MKL speed-up over naive: {} x\n'.format(mklTime / naiveTime))
+        print('MKL speed-up over naive: {} x\n'.format(mklTime / naiveTime))
+
+if __name__ == "__main__":
+    benchmark()
