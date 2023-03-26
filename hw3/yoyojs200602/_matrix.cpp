8f39f24a52fb62a2f0a4125940ae295d54222148
yoyojs200602 <yoyochang24@gmail.com>
2023-03-26 13:42:00 +0800

    check function without mkl

--- hw3/yoyojs200602/_matrix.cpp
+++ hw3/yoyojs200602/_matrix.cpp
@@ -15,8 +15,8 @@ public:
 
     double& operator()(int i, int j) { return data_[i * ncol_ + j]; }
     const double& operator()(int i, int j) const { return data_[i * ncol_ + j]; }
-    double* data() { return data_.data();}
-    const double* data() const { return data_.data();}
+    double* data() { return data_.data(); }
+    const double* data() const { return data_.data(); }
 
 private:
     int nrow_, ncol_;
@@ -66,28 +66,28 @@ Matrix multiply_tile(const Matrix& a, const Matrix& b, int tile_size) {
 }
 
 Matrix multiply_mkl(const Matrix& a, const Matrix& b) {
-    Matrix c(a.nrow(), b.ncol());
-
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-                a.nrow(), b.ncol(), a.ncol(), 1.0,
-                a.data(), a.ncol(), b.data(), b.ncol(),
-                0.0, c.data(), c.ncol());
-
-    return c;
-
     // Matrix c(a.nrow(), b.ncol());
 
-    // for (int i = 0; i < a.nrow(); ++i) {
-    //     for (int j = 0; j < b.ncol(); ++j) {
-    //         double sum = 0.0;
-    //         for (int k = 0; k < a.ncol(); ++k) {
-    //             sum += a(i, k) * b(k, j);
-    //         }
-    //         c(i, j) = sum;
-    //     }
-    // }
+    // cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+    //             a.nrow(), b.ncol(), a.ncol(), 1.0,
+    //             a.data(), a.ncol(), b.data(), b.ncol(),
+    //             0.0, c.data(), c.ncol());
 
     // return c;
+
+    Matrix c(a.nrow(), b.ncol());
+
+    for (int i = 0; i < a.nrow(); ++i) {
+        for (int j = 0; j < b.ncol(); ++j) {
+            double sum = 0.0;
+            for (int k = 0; k < a.ncol(); ++k) {
+                sum += a(i, k) * b(k, j);
+            }
+            c(i, j) = sum;
+        }
+    }
+
+    return c;
 }
 
 
