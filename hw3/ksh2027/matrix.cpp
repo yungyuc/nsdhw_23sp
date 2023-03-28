02a8d5bb173654992dcbff754e3db2f751b160e5
ksh2027 <kch772018@gmail.com>
2023-03-28 19:05:47 +0800

    fix matrix.cpp

--- hw3/ksh2027/matrix.cpp
+++ hw3/ksh2027/matrix.cpp
@@ -9,16 +9,16 @@ class Matrix {
 
 public:
 
-    Matrix(size_t nrow, size_t ncol) : m_nrow(nrow), m_ncol(ncol), data(nrow * ncol){}
+    Matrix(size_t nrow, size_t ncol) : m_nrow(nrow), m_ncol(ncol), data_v(nrow * ncol){}
 
     ~Matrix(){}
 
     // No bound check.
     double   operator() (size_t row, size_t col) const{
-        return data[row*m_ncol + col];
+        return data_v[row*m_ncol + col];
     }
     double & operator() (size_t row, size_t col){
-        return data[row*m_ncol + col];
+        return data_v[row*m_ncol + col];
     }
     bool operator == (const Matrix& other) const{
         if(nrow() != other.nrow() || ncol() != other.ncol()) {return 0;}
@@ -34,12 +34,13 @@ public:
 
     size_t nrow() const { return m_nrow; }
     size_t ncol() const { return m_ncol; }
+    const double* data() const { return data_v.data(); }
 
 private:
 
     size_t m_nrow;
     size_t m_ncol;
-    std::vector<double> data;
+    std::vector<double> data_v;
 
 };
 
@@ -49,10 +50,10 @@ Matrix multiply_naive(Matrix const & A, Matrix const & B) {
     }
 
     Matrix C(A.nrow(), B.ncol());
-    for (int i = 0; i < A.nrow(); i++) {
-        for (int j = 0; j < B.ncol(); j++) {
+    for (size_t i = 0; i < A.nrow(); i++) {
+        for (size_t j = 0; j < B.ncol(); j++) {
             double sum = 0.0;
-            for (int k = 0; k < A.ncol(); k++) {
+            for (size_t k = 0; k < A.ncol(); k++) {
                 sum += A(i,k) * B(k,j);
             }
             C(i,j) = sum;
@@ -67,13 +68,13 @@ Matrix multiply_tile(const Matrix& A, const Matrix& B, size_t tile_size) {
     }
 
     Matrix C(A.nrow(), B.ncol());
-    for (int i = 0; i < A.nrow(); i += tile_size) {
-        for (int j = 0; j < B.ncol(); j += tile_size) {
-            for (int k = 0; k < A.ncol(); k += tile_size) {
-                for (int ii = i; ii < std::min(i + tile_size, A.nrow()); ii++) {
-                    for (int jj = j; jj < std::min(j + tile_size, B.ncol()); jj++) {
+    for (size_t i = 0; i < A.nrow(); i += tile_size) {
+        for (size_t j = 0; j < B.ncol(); j += tile_size) {
+            for (size_t k = 0; k < A.ncol(); k += tile_size) {
+                for (size_t ii = i; ii < std::min(i + tile_size, A.nrow()); ii++) {
+                    for (size_t jj = j; jj < std::min(j + tile_size, B.ncol()); jj++) {
                         double sum = 0.0;
-                        for (int kk = k; kk < std::min(k + tile_size, A.ncol()); kk++) {
+                        for (size_t kk = k; kk < std::min(k + tile_size, A.ncol()); kk++) {
                             sum += A(ii,kk) * B(kk,jj);
                         }
                         C(ii,jj) += sum;
@@ -91,7 +92,7 @@ Matrix multiply_mkl(const Matrix& A, const Matrix& B) {
     }
 
     Matrix C(A.nrow(), B.ncol());
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, A.nrow(), B.ncol(), A.ncol(), 1.0, A.data.data(), A.ncol(), B.data.data(), B.ncol(), 0.0, C.data.data(), C.ncol());
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, A.nrow(), B.ncol(), A.ncol(), 1.0, A.data(), A.ncol(), B.data(), B.ncol(), 0.0, C.data(), C.ncol());
     return C;
 }
 
@@ -108,5 +109,5 @@ PYBIND11_MODULE(_matrix, m) {
 
     m.def("multiply_naive", &multiply_naive, "naive matrix-matrix multiplication");
     m.def("multiply_tile", &multiply_tile, "matrix-matrix multiplication with tiling");
-    // m.def("multiply_mkl", &multiply_mkl, "multiplication by using DGEMM");
+    m.def("multiply_mkl", &multiply_mkl, "multiplication by using DGEMM");
 }
