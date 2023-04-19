aa077aeff8e13fa4231ebe6243660dffbbad42f1
Calvin9c <m.acc.ch1230@gmail.com>
2023-04-19 08:31:06 +0800

    Trial and Error

--- hw1/Calvin9c/countline.sh
+++ hw1/Calvin9c/countline.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
+if [ "$#" -lt 1 ]; then
   echo "missing file name"
-elif [ $# -gt 1 ]; then
+elif [ "$#" -gt 1 ]; then
   echo "only one argument is allowed"
 else
-  if [ -f $1 ]; then
-      num_lines=$( cat $1 | wc -l)
+  if [ -f "$1" ]; then
+      num_lines=$( cat "$1" | wc -l)
       echo "${num_lines} lines in $1"
   else
       echo "$1 not found"
