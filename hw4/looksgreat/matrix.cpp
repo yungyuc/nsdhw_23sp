68db397621406bf47d30f1d5fe65ad391ff57df1
looksgreat <lemonade.cs07@nctu.edu.tw>
2023-04-26 01:36:04 +0800

    fix

--- hw4/looksgreat/matrix.cpp
+++ hw4/looksgreat/matrix.cpp
@@ -13,74 +13,83 @@ template <class T>
 class CustomAllocator{
 public:
     using value_type = T;
-    CustomAllocator() : m_allocated(0), m_deallocated(0){}
-    int bytes(){
-        return m_allocated - m_deallocated;
-    }
-    T* allocate(int n){
+    CustomAllocator() = default;
+    
+    T* allocate(size_t n){
+        if (n > numeric_limits<size_t>::max()/sizeof(T)) throw bad_alloc();
         m_allocated += n*sizeof(T);
-        return new T[n];
+        m_byte += n*sizeof(T);
+        T *ret = (T *)(malloc(n*sizeof(T)));
+        if(ret == nullptr)
+            throw std::bad_alloc();
+        return ret;
     }
-    void deallocate(T *ptr, int n){
+    void deallocate(T *ptr, size_t n){
         m_deallocated += n*sizeof(T);
-        delete[] ptr;
+        m_byte -= n*sizeof(T);
+        free(ptr);
+    }
+    static size_t allocated(){
+        return m_allocated;
+    }
+    static size_t deallocated(){
+        return m_deallocated;
+    }
+    static size_t bytes(){
+        return m_byte;
     }
 private:
-    int m_allocated, m_deallocated;
-}
+    static size_t m_allocated, m_deallocated , m_byte;
+};
+
+template <class T> size_t CustomAllocator<T>::m_allocated = 0;
+template <class T> size_t CustomAllocator<T>::m_deallocated = 0;
+template <class T> size_t CustomAllocator<T>::m_byte = 0;
 
 
 class Matrix {
-
-
 public:
-    Matrix(int rows, int cols) : m_rows(rows), m_cols(cols), m_data(new double[rows*cols]){
-        memset(m_data, 0, sizeof(double)*rows*cols);
+    Matrix(): m_rows(0), m_cols(0) {}
+    Matrix(int nrow, int ncol): m_rows(nrow), m_cols(ncol), m_data(nrow * ncol) {}
+    double& operator()(int i, int j) {
+        return m_data[i*m_cols+j];
     }
-    Matrix(const Matrix &m) : m_cols(m.ncol()), m_rows(m.nrow()){
-        m_data = new double[m_rows*m_cols];
-        for (int i = 0; i < m_rows; i++){
-            for (int j = 0; j < m_cols; j++){
-                m_data[i*m_cols+j] = m(i, j);
-            }
-        }
+    const double& operator()(int i, int j) const {
+        return m_data[i*m_cols+j];
     }
-    ~Matrix(){
-        delete[] m_data;
-    }
-    double &operator()(int x, int y){
-        return m_data[y*m_cols+x];
-    }
-    double operator()(int x, int y) const{
-        return m_data[y*m_cols+x];
-    }
-    bool operator ==(const Matrix &m) const{
-        if (m_rows != m.nrow() || m_cols != m.ncol()){
+    
+    bool operator ==(const Matrix &m) const
+    {
+        if (m_rows != m.nrow() || m_cols != m.ncol())
             return false;
-        }
-        for (int i = 0; i < m_rows; i++){
-            for (int j = 0; j < m_cols; j++){
-                if ((*this)(i, j) != m(i, j)){
+
+        for (int i = 0; i < m_rows; i++)
+        {
+            for (int j = 0; j < m_cols; j++)
+            {
+                if (this->operator()(i, j) != m(i, j))
                     return false;
-                }
             }
         }
+
         return true;
     }
-    int nrow() const{ 
+    int nrow() const { 
         return m_rows; 
     }
-    int ncol() const{ 
-        return m_cols;
+    int ncol() const { 
+        return m_cols; 
     }
-    double* data() const{
-        return m_data;
+    double* data() { 
+        return m_data.data(); 
+    }
+    const double* data() const { 
+        return m_data.data(); 
     }
 
 private:
-    int m_rows;
-    int m_cols;
-    vector<double, CustomAllocator<double>> m_data;
+    int m_rows, m_cols;
+    std::vector<double, CustomAllocator<double>> m_data;
 };
 
 Matrix multiply_naive(Matrix const &m1, Matrix const &m2){
@@ -111,9 +120,11 @@ Matrix multiply_tile(Matrix const &m1, Matrix const &m2, int const tile_size){
                 int kmax = std::min(k0 + tile_size, k);
                 for (int i = i0; i < imax; i++) {
                     for (int j = j0; j < jmax; j++) {
+                        double sum = 0.0;
                         for (int l = k0; l < kmax; l++) {
-                            ret(i, j) += m1(i, l) * m2(l, j);
+                            sum += m1(i, l) * m2(l, j);
                         }
+                        ret(i, j) += sum;
                     }
                 }
             }
@@ -128,7 +139,7 @@ Matrix multiply_mkl(Matrix const &m1, Matrix const &m2){
     mkl_set_num_threads(1);
     Matrix ret(m1.nrow(), m2.ncol());
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m1.nrow(),  m2.ncol(), m1.ncol(), 1.0 , m1.data(),
-     m1.ncol(), m2.data(), m2.ncol(), 0.0, ret.data(), ret.ncol());
+     m1.ncol(), m2.data(), m2.ncol(), 0.0, const_cast<double*>(ret.data()), ret.ncol());
     return ret;
 }
 
@@ -142,7 +153,10 @@ PYBIND11_MODULE(_matrix, m){
     m.def("allocated", &CustomAllocator<double>::allocated);
     m.def("deallocated", &CustomAllocator<double>::deallocated);
     py::class_<Matrix>(m, "Matrix")
+        .def(py::init<>())
         .def(py::init<int, int>())
+        .def("data", pybind11::overload_cast<>(&Matrix::data))
+        .def("data", pybind11::overload_cast<>(&Matrix::data, pybind11::const_))
         .def_property_readonly("nrow", [](const Matrix &mat) { return mat.nrow(); })
         .def_property_readonly("ncol", [](const Matrix &mat) { return mat.ncol(); })
         .def("__eq__", [](const Matrix &a, const Matrix &b) { 
