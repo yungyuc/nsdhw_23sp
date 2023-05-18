55f2f35a06343d6dacdaa4c00e066fd7b46d6367
looksgreat <lemonade.cs07@nctu.edu.tw>
2023-05-18 16:04:59 +0800

    t

--- hw5/looksgreat/matrix.hpp
+++ hw5/looksgreat/matrix.hpp
@@ -9,13 +9,13 @@ class Matrix {
 
 
 public:
-    Matrix(int rows, int cols) : m_rows(rows), m_cols(cols), m_data(new double[rows*cols]){
+    Matrix(size_t rows, size_t cols) : m_rows(rows), m_cols(cols), m_data(new double[rows*cols]){
         memset(m_data, 0, sizeof(double)*rows*cols);
     }
     Matrix(const Matrix &m) : m_cols(m.ncol()), m_rows(m.nrow()){
         m_data = new double[m_rows*m_cols];
-        for (int i = 0; i < m_rows; i++){
-            for (int j = 0; j < m_cols; j++){
+        for (size_t i = 0; i < m_rows; i++){
+            for (size_t j = 0; j < m_cols; j++){
                 m_data[i*m_cols+j] = m(i, j);
             }
         }
@@ -23,18 +23,18 @@ public:
     ~Matrix(){
         delete[] m_data;
     }
-    double &operator()(int x, int y){
+    double &operator()(size_t x, size_t y){
         return m_data[y*m_cols+x];
     }
-    double operator()(int x, int y) const{
+    double operator()(size_t x, size_t y) const{
         return m_data[y*m_cols+x];
     }
     bool operator ==(const Matrix &m) const{
         if (m_rows != m.nrow() || m_cols != m.ncol()){
             return false;
         }
-        for (int i = 0; i < m_rows; i++){
-            for (int j = 0; j < m_cols; j++){
+        for (size_t i = 0; i < m_rows; i++){
+            for (size_t j = 0; j < m_cols; j++){
                 if ((*this)(i, j) != m(i, j)){
                     return false;
                 }
@@ -42,10 +42,10 @@ public:
         }
         return true;
     }
-    int nrow() const{ 
+    size_t nrow() const{ 
         return m_rows; 
     }
-    int ncol() const{ 
+    size_t ncol() const{ 
         return m_cols;
     }
     double* data() const{
@@ -53,17 +53,17 @@ public:
     }
 
 private:
-    int m_rows;
-    int m_cols;
+    size_t m_rows;
+    size_t m_cols;
     double *m_data;
 };
 
 Matrix multiply_naive(Matrix const &m1, Matrix const &m2){
     Matrix ret(m1.nrow(), m2.ncol());
-    for (int i = 0; i < m1.nrow(); i++) {
-        for (int j = 0; j < m2.ncol(); j++) {
+    for (size_t i = 0; i < m1.nrow(); i++) {
+        for (size_t j = 0; j < m2.ncol(); j++) {
             double sum = 0.0;
-            for (int k = 0; k < m1.ncol(); k++) {
+            for (size_t k = 0; k < m1.ncol(); k++) {
                 sum += m1(i, k) * m2(k, j);
             }
             ret(i, j) = sum;
@@ -72,21 +72,21 @@ Matrix multiply_naive(Matrix const &m1, Matrix const &m2){
     return ret;
 }
 
-Matrix multiply_tile(Matrix const &m1, Matrix const &m2, int const tile_size){
-    int m = m1.nrow();
-    int n = m2.ncol();
-    int k = m1.ncol();
+Matrix multiply_tile(Matrix const &m1, Matrix const &m2, size_t const tile_size){
+    size_t m = m1.nrow();
+    size_t n = m2.ncol();
+    size_t k = m1.ncol();
 
     Matrix ret(m, n);
-    for (int i0 = 0; i0 < m; i0 += tile_size) {
-        int imax = std::min(i0 + tile_size, m);
-        for (int j0 = 0; j0 < n; j0 += tile_size) {
-            int jmax = std::min(j0 + tile_size, n);
-            for (int k0 = 0; k0 < k; k0 += tile_size) {
-                int kmax = std::min(k0 + tile_size, k);
-                for (int i = i0; i < imax; i++) {
-                    for (int j = j0; j < jmax; j++) {
-                        for (int l = k0; l < kmax; l++) {
+    for (size_t i0 = 0; i0 < m; i0 += tile_size) {
+        size_t imax = std::min(i0 + tile_size, m);
+        for (size_t j0 = 0; j0 < n; j0 += tile_size) {
+            size_t jmax = std::min(j0 + tile_size, n);
+            for (size_t k0 = 0; k0 < k; k0 += tile_size) {
+                size_t kmax = std::min(k0 + tile_size, k);
+                for (size_t i = i0; i < imax; i++) {
+                    for (size_t j = j0; j < jmax; j++) {
+                        for (size_t l = k0; l < kmax; l++) {
                             ret(i, j) += m1(i, l) * m2(l, j);
                         }
                     }
